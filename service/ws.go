12676511f7b420b32571a8fab3f9dbda4cc5e93f
hugopukito <gogo26hugop@gmail.com>
2023-09-27 01:42:38 +0200

    ws can send data in json, but can't receive it

--- service/ws.go
+++ service/ws.go
@@ -1,7 +1,7 @@
 package service
 
 import (
-	"fmt"
+	"io"
 	"log"
 	"net/http"
 
@@ -15,6 +15,7 @@ var upgrader = websocket.Upgrader{
 }
 
 var clients = make(map[*websocket.Conn]any)
+var bdcast = make(chan string)
 
 func HandleWsConnection(w http.ResponseWriter, r *http.Request) {
 	ws, err := upgrader.Upgrade(w, r, nil)
@@ -24,5 +25,49 @@ func HandleWsConnection(w http.ResponseWriter, r *http.Request) {
 	defer ws.Close()
 
 	clients[ws] = struct{}{}
-	fmt.Println("user connected")
+
+	for {
+		var buff map[string]any
+		err := ws.ReadJSON(&buff)
+		if _, ok := buff["test"]; ok {
+			bdcast <- "<div id='messageContent' hx-swap-oob='true'>Message: </div>"
+		}
+
+		if err != nil {
+			delete(clients, ws)
+			break
+		}
+	}
+}
+
+func handleMessages() {
+	for {
+		msg := <-bdcast
+		messageClients(msg)
+	}
+}
+
+func messageClients(msg string) {
+	for client := range clients {
+		messageClient(client, msg)
+	}
+}
+
+func messageClient(client *websocket.Conn, msg string) {
+	err := client.WriteJSON(msg)
+
+	if err != nil && unsafeError(err) {
+		log.Printf("error: %v", err)
+		client.Close()
+		delete(clients, client)
+	}
+}
+
+// If a message is sent while a client is closing, ignore the error
+func unsafeError(err error) bool {
+	return !websocket.IsCloseError(err, websocket.CloseGoingAway) && err != io.EOF
+}
+
+func init() {
+	go handleMessages()
 }
