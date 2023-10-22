e67ce7308e8ec9cf89cd8f44d7038081a440b370
hugopukito <gogo26hugop@gmail.com>
2023-10-22 18:43:48 +0200

    rm in go code redis unused

--- main.go
+++ main.go
@@ -2,24 +2,21 @@ package main
 
 import (
 	"htmx/router"
-	"log"
-
-	"github.com/go-redis/redis"
 )
 
 func main() {
 	//fixture.RunFixtures("fixtures", database.DatabaseParams{Name: "htmx"})
 
-	opt, err := redis.ParseURL("redis://redis:6379")
-	if err != nil {
-		log.Println("error connection redis parse")
-	}
-	redisClient := redis.NewClient(opt)
-	if err := redisClient.Ping(); err.String() != "ping: PONG" {
-		log.Println("error redis connection init: " + err.String())
-	} else {
-		log.Println("redis connection worked")
-	}
+	// opt, err := redis.ParseURL("redis://redis:6379")
+	// if err != nil {
+	// 	log.Println("error connection redis parse")
+	// }
+	// redisClient := redis.NewClient(opt)
+	// if err := redisClient.Ping(); err.String() != "ping: PONG" {
+	// 	log.Println("error redis connection init: " + err.String())
+	// } else {
+	// 	log.Println("redis connection worked")
+	// }
 
 	router.InitRouter()
 }
